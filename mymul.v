7b06f5ee278ba2e980ad9a18410d50076c58a809
Zhzhbhs <141635757+Zhzhbhs@users.noreply.github.com>
2025-12-31 11:12:55 +0800

    Update mymul.v

--- mymul.v
+++ mymul.v
@@ -1,25 +1,21 @@
-/**
- * @Author:zht、szw
- * @Date: 2021-12-10
- */
 
 `include "lib/defines.vh"
 
 module mymul(
-	input wire rst,							//复位
-	input wire clk,							//时钟
-	input wire signed_mul_i,				//是否为有符号乘法运算，1位有符号
-	input wire[31:0] a_o,				//被乘数
-	input wire[31:0] b_o,				//乘数
-	input wire start_i,						//是否开始乘法运算
-	output reg[63:0] result_o,				//乘法运算结果
-	output reg ready_o						//乘法运算是否结束
+	input wire rst,							
+	input wire clk,						
+	input wire signed_mul_i,				
+	input wire[31:0] a_o,				
+	input wire[31:0] b_o,				
+	input wire start_i,						
+	output reg[63:0] result_o,				
+	output reg ready_o						
 );
 reg [31:0] temp_opa,temp_opb;
 reg [63:0] pv;
 reg [63:0] ap;
-reg [5:0] i;//进行到第几位
-reg [1:0] state;// 00:空闲  10:开始   11:结束
+reg [5:0] i;
+reg [1:0] state;
 
 always @ (posedge clk) begin
 		if (rst) begin
@@ -32,12 +28,12 @@ always @ (posedge clk) begin
                     if (start_i== `MulStart) begin
                         state <= `MulOn;
                         i <= 6'b00_0000;
-					    if(signed_mul_i == 1'b1 && a_o[31] == 1'b1) begin			//被乘数为负数
+					    if(signed_mul_i == 1'b1 && a_o[31] == 1'b1) begin			
 							temp_opa = ~a_o + 1;
 						end else begin
 							temp_opa = a_o;
 						end
-						if(signed_mul_i == 1'b1 && b_o[31] == 1'b1 ) begin			//乘数除数为负数
+						if(signed_mul_i == 1'b1 && b_o[31] == 1'b1 ) begin			
 								temp_opb = ~b_o + 1;
 						end else begin
 							temp_opb = b_o;
@@ -49,7 +45,7 @@ always @ (posedge clk) begin
                     end
 				end				
 				
-				`MulOn: begin				//乘法运算
+				`MulOn: begin				
                         if(i != 6'b100000) begin
                             if(temp_opb[0]==1'b1) begin
 								pv <= pv + ap;
@@ -72,7 +68,7 @@ always @ (posedge clk) begin
 					   
 				end
 				
-				`MulEnd: begin			//乘法结束
+				`MulEnd: begin			
 					result_o <= pv;
 					ready_o <= `MulResultReady;
 					if (start_i == `MulStop) begin
@@ -87,3 +83,4 @@ always @ (posedge clk) begin
 	end
 
 endmodule
+
